c1e38c8f3b15eee71289f77acc7dd42369efd316
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-08-06 21:44:36 +0530

    Revert "database conn"
    This reverts commit 69e3ef9b28afc0f1bf744dc180b08d584231e9a6.

--- pollme/settings.py
+++ pollme/settings.py
@@ -83,9 +83,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['defult'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators

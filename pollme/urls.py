821380a6bc205c3695d3c8fae150cf285ec7fe0f
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-08-16 14:17:07 +0530

    Update urls.py

--- pollme/urls.py
+++ pollme/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('panel/', admin.site.urls),
     path('', views.home, name='home'),
     path('polls/', include('polls.urls', namespace="polls")),
     path('accounts/', include('accounts.urls', namespace="accounts")),

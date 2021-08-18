4792fa5f12786baba68818a0db1bdd59f3ff8b9e
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-08-18 14:30:17 +0530

    Update settings.py

--- pollme/settings.py
+++ pollme/settings.py
@@ -76,18 +76,10 @@ WSGI_APPLICATION = 'pollme.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
- 'default': {
-
-       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+     'default': {
+         
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
         'NAME': 'daavloocvf1onh',
 

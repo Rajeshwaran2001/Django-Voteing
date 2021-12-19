6836cc138ce3b7232d480a5682aa805c3838c871
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2021-12-19 14:59:03 +0530

    Update settings.py

--- pollme/settings.py
+++ pollme/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'x*za6xf&_80ofdpae!yzq61g9ffikkx9$*iygbl$j7rr4wlf8t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["172.31.82.253"]
+ALLOWED_HOSTS = ["44.201.219.181","ec2-44-201-219-181.compute-1.amazonaws.com"]
 
 
 # Application definition
